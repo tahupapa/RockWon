b00915741c9bc4ffa5f1cce9c672275a83bbe69d
tahupapa <xognl3164@gmail.com>
2024-02-03 14:21:56 +0900

    글로벌 스타일(리셍) 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import Router from "./router/Router";
+import GlobalStyle from "./GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={Router} />
   </React.StrictMode>
 );
